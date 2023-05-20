ea996c47edda4438efb04d1c16f2d0ad06b52b3c
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-05-20 14:44:51 +0200

    Update pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,8 @@
+## this will generate a .zip file with all necessary contents and store it to your Windows Downloads folder
+# Note: remove the "config" or "plugins" elements from "Path = ..." if not used
 $TargetZipFileName = "~\Downloads\$(Split-Path -Path $PSScriptRoot -Leaf).zip"
 $compress = @{
-  Path             = "$PSScriptRoot\README.md", "$PSScriptRoot\CHANGELOG.md", "$PSScriptRoot\manifest.json", "$PSScriptRoot\icon.png", "$PSScriptRoot\config"
+  Path             = "$PSScriptRoot\README.md", "$PSScriptRoot\CHANGELOG.md", "$PSScriptRoot\manifest.json", "$PSScriptRoot\icon.png", "$PSScriptRoot\config", "$PSScriptRoot\plugins"
   CompressionLevel = "Fastest"
   DestinationPath  = $TargetZipFileName
 }
